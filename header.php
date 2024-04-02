f759ffcc89c07bca096341f51acecdebb9cbf200
Reza Sadid <rezasadid753@gmail.com>
2024-04-02 18:32:38 +0330

    Enhanced HTML structure to accommodate Farsi language support and integrated PHP functionality

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa-IR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#000">
+    <meta name="theme-color" content="#000" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.svg">
+    <title>ام تحریر</title>
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <header>
+    </header>
+    <div class="container">
