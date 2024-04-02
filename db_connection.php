f0ddb0844f1faef15789e1012980a0d74f689843
Reza Sadid <rezasadid753@gmail.com>
2024-04-02 18:33:42 +0330

    Implemented PHP retrieval of database content for dynamic JavaScript rendering

---
+++ db_connection.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection settings
+define('DB_HOST', 'localhost');
+define('DB_USER', 'database_name_goes_here');
+define('DB_PASSWORD', 'user_password_goes_here');
+define('DB_NAME', 'user_name_goes_here');
+?>
--- index.php
+++ index.php
@@ -1,16 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"> 
-    <title>Document</title>
-</head>
-<body>
+<?php
+// Include Header
+include 'header.php';
+
+// Database connection
+include 'db_connection.php';
+
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die('ارتباط با پایگاه داده ناموفق بود: ' . $conn->connect_error);
+}
+
+// Retrieve words from the database
+$sql = "SELECT * FROM words";
+$result = $conn->query($sql);
+
+$words = array();
+
+if ($result->num_rows > 0) {
+    // Store words in an array
+    while ($row = $result->fetch_assoc()) {
+        $words[] = $row["persian_word"];
+    }
+} else {
+    echo "هیچ کلمه ای در پایگاه داده یافت نشد";
+}
+
+$conn->close();
+?>
+
+<script>
+    // Pass words from database to word variable in js
+    var words = <?php echo json_encode($words); ?>;
+</script>
+
     <div id="word"></div>
     <div id="typed"></div>
     <!-- <input type="text" id="userInput">
     <button onclick="checkWord()">Submit</button> -->
-    <script src="script.js"></script>
-</body>
-</html>
+    <script src="game.js"></script>
+
+<?php
+// Include Footer
+include 'footer.php';
+?>
